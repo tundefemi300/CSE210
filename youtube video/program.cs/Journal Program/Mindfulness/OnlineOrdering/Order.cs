e1c842acca2ecce20468e9b4c923fcdc2a94d70f
Akanbi Oluwafemi <tundefemi300@gmail.com>
2026-06-15 23:09:20 +0100

    Complete W04 Online Ordering Program

---
+++ youtube video/program.cs/Journal Program/Mindfulness/OnlineOrdering/Order.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Text;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        total += _customer.LivesInUSA() ? 5 : 35;
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        StringBuilder label = new StringBuilder();
+
+        foreach (Product product in _products)
+        {
+            label.AppendLine(
+                $"Product: {product.GetName()} | ID: {product.GetProductId()}");
+        }
+
+        return label.ToString();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
