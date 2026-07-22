4d0c6d738b06f93b7f5de0869c16351dc4740ea9
Akanbi Oluwafemi <tundefemi300@gmail.com>
2026-07-22 12:34:52 +0100

    Complete assignment

--- Scripture Memorizer/ScriptureMemorizer/obj/Debug/net10.0/ScriptureMemorizer.AssemblyInfo.cs
+++ Scripture Memorizer/ScriptureMemorizer/obj/Debug/net10.0/ScriptureMemorizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScriptureMemorizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ffec7152fef5ea22f9ea0f637e3aa891d6fedf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06243337ddb0b5966995d6b4cadcba93652815f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScriptureMemorizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScriptureMemorizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
